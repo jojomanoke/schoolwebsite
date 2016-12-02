c0955537f2de89c2f85434d6880582021987e5a1
jojomanoke <sylvanoverkuyl@hotmail.com>
2016-12-02 10:37:20 +0100

    startpagina

---
+++ Start.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang=nl>
+<head>
+	<meta charset=utf-8>
+	<meta name=description content="beschrijving van de webpagina">
+	<meta name=keywords content="trefwoorden, gescheiden, door, komma's">
+	<link rel="stylesheet" type="text/css" href="start.css">
+	<title>Titel van de webpagina</title>
+</head>
+<body>
+
+<ul class="navbar">
+  <li class="navbar" class="actief"><a href="DutchFlevoGamers.html">Home</a></li>
+  <li class="navbar"><a href="https://www.youtube.com/channel/UCqD49gmejgN8gOzjQX06GAA">Youtube</a></li>
+  <li class="navbar"><a href="contact.html">Contact</a></li>
+  <li class="navbar"><a href="https://plus.google.com/111470645161528284138/about">Google +</a></li>
+  <li class="navbar" class=about><a href="about.html">Over ons</a></li>
+</ul>
+
+<br>
+<br>
+
+
+</body>
+</html>
